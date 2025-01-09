d6c711d86c6b2768106c6efbf3d75fe26f9184cd
Pabitha1045 <153606235+Pabitha1045@users.noreply.github.com>
2025-01-09 08:51:25 +0530

    Add files via upload

---
+++ DOM.js
@@ -0,0 +1,14 @@
+// // 
+
+// // let body=document.getElementById("data")
+// // body.children[0].remove()
+
+// let date=new Date()
+// console.log(date.getDate());
+// console.log(date.getHours());
+// console.log(date.getSeconds());
+
+child2.addEventListener("click",event)
+const remove=()=>{
+    child2.removeEventListener("click",event1)
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const data = async() => {
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    response=await response.json()
+    console.log(response);
+};
+
+data()
